4977af102158810e8c1f629f7a7657e2c08fe6b5
johnramosf <johnalex.ramos@gmail.com>
2024-10-09 23:39:25 -0500

    MyFirtsComponent

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,14 @@
+export const MyFirstComponent = () => {
+  return (
+    <>
+      <div>MyFirstComponent</div>
+      <h1>Temas de React</h1>
+      <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados Hooks</li>
+        <li>Props</li>
+      </ul>
+    </>
+  );
+};
